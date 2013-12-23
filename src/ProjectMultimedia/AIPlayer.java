aa38c8e385bfea3137ad8c75a5cac9d3d19338a0
segmeton ori <segmeton ori@albertus-pc>
2013-12-23 17:12:42 +0700

    add aiplayer

---
+++ src/ProjectMultimedia/AIPlayer.java
@@ -0,0 +1,24 @@
+package ProjectMultimedia;
+
+public abstract class AIPlayer {
+   protected int ROWS = GameMain.ROWS;  // number of rows
+   protected int COLS = GameMain.COLS;  // number of columns
+ 
+   protected Cell[][] cells; // the board's ROWS-by-COLS array of Cells
+   protected Seed mySeed;    // computer's seed
+   protected Seed oppSeed;   // opponent's seed
+ 
+   /** Constructor with reference to game board */
+   public AIPlayer(Board board) {
+      cells = board.cells;
+   }
+ 
+   /** Set/change the seed used by computer and opponent */
+   public void setSeed(Seed seed) {
+      this.mySeed = seed;
+      oppSeed = (mySeed == Seed.CROSS) ? Seed.NOUGHT : Seed.CROSS;
+   }
+ 
+   /** Abstract method to get next move. Return int[2] of {row, col} */
+   abstract int[] move();  // to be implemented by subclasses
+}
